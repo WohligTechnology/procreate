04618276e265b8415618e39bed4b8bb77f0bc05e
Chintan <chintan@wohlig.com>
2017-05-16 20:10:51 +0530

    ok

--- frontend/js/production.js
+++ frontend/js/production.js
@@ -66064,7 +66064,27 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locat
             url: "/form",
             templateUrl: tempateURL,
             controller: 'FormCtrl'
-        });
+        })
+
+          .state('about', {
+            url: "/about",
+            templateUrl: tempateURL,
+            controller: 'AboutCtrl'
+        })
+
+           .state('offering', {
+            url: "/offering",
+            templateUrl: tempateURL,
+            controller: 'OfferingCtrl'
+        })
+
+
+         .state('approach', {
+            url: "/approach",
+            templateUrl: tempateURL,
+            controller: 'ApproachCtrl'
+        })
+        
     $urlRouterProvider.otherwise("/");
     $locationProvider.html5Mode(isproduction);
 });
@@ -66268,7 +66288,7 @@ myApp.factory('apiService', function ($http, $q, $timeout) {
 
     };
 });
-myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout,$rootScope) {
         $scope.template = TemplateService.getHTML("content/home.html");
         TemplateService.title = "Home"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
@@ -66279,6 +66299,21 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         //     'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
         //     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
         // ];
+
+//         $scope.initMap = function() {
+//            var uluru = {lat: -25.363, lng: 131.044};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 15,
+//           center: uluru
+//         });
+//         var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+//  };
+
+//  google.maps.event.addDomListener(window, 'load', $scope.initMap);  
+
 
    $scope.inspireImg = [{
         img: 'frontend/img/homepage/home3.jpg'
@@ -66454,12 +66489,36 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         };
     })
 
+        .controller('AboutCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.getHTML("content/about.html");
+        TemplateService.title = "About"; //This is the Title of the Website
+         TemplateService.header = "views/template/header1.html"; //This is the Title of the Website
+        $scope.navigation = NavigationService.getNavigation();
+    })
+
+     .controller('OfferingCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.getHTML("content/offering.html");
+        TemplateService.title = "Offering"; //This is the Title of the Website
+        $scope.navigation = NavigationService.getNavigation();
+    })
+
+    
     //Example API Controller
     .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
         apiService.getDemo($scope.formData, function (data) {
             console.log(data);
         });
     });
+myApp.controller('ApproachCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.getHTML("content/approach.html");
+        TemplateService.title = "Approach"; //This is the Title of the Website
+        $scope.navigation = NavigationService.getNavigation();
+        $scope.formSubmitted = false;
+        $scope.submitForm = function (data) {
+            console.log(data);
+            $scope.formSubmitted = true;
+        };
+    })
 myApp.controller('headerCtrl', function ($scope, TemplateService) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
