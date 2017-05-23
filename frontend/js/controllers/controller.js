59145407a9f7aaf9b23be2f1759c96249aaa7953
sohan1 <shn619@gmail.com>
2017-05-23 19:59:05 +0530

    map

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -1,4 +1,5 @@
-myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout,$rootScope) {
+var initMap = function () {};
+myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, $rootScope) {
         $scope.template = TemplateService.getHTML("content/home.html");
         TemplateService.title = "Home"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
@@ -10,168 +11,168 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         //     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
         // ];
 
-//         $scope.initMap = function() {
-//            var uluru = {lat: -25.363, lng: 131.044};
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 15,
-//           center: uluru
-//         });
-//         var marker = new google.maps.Marker({
-//           position: uluru,
-//           map: map
-//         });
-//  };
+        //         $scope.initMap = function() {
+        //            var uluru = {lat: -25.363, lng: 131.044};
+        //         var map = new google.maps.Map(document.getElementById('map'), {
+        //           zoom: 15,
+        //           center: uluru
+        //         });
+        //         var marker = new google.maps.Marker({
+        //           position: uluru,
+        //           map: map
+        //         });
+        //  };
 
-//  google.maps.event.addDomListener(window, 'load', $scope.initMap);  
+        //  google.maps.event.addDomListener(window, 'load', $scope.initMap);  
 
 
-   $scope.inspireImg = [{
-        img: 'frontend/img/homepage/home3.jpg'
-    
-    }, {
-        img: 'frontend/img/homepage/home3.jpg'
- 
-    },
-    {
-        img: 'frontend/img/homepage/home3.jpg'
+        $scope.inspireImg = [{
+                img: 'frontend/img/homepage/home3.jpg'
 
-    },
-    {
-        img: 'frontend/img/homepage/home3.jpg'
-      
-    },
-    {
-        img: 'frontend/img/homepage/home3.jpg'
-     
-    },
-    {
-        img: 'frontend/img/homepage/home3.jpg',
-        titlehome2: 'THE TWO-DOLLAR BILL',
-        title2: 'BY CHRISTIAN GREWE'
-    }
-    ]
+            }, {
+                img: 'frontend/img/homepage/home3.jpg'
 
- $scope.homeImg = [{
-        img: 'frontend/img/homepage/32.png'
-    
-    },
- {
-        img: 'frontend/img/homepage/42.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/61.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/82.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/10.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/122.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/43.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/52.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/73.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/82.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/111.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/31.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/32.png'
-    
-    },
- {
-        img: 'frontend/img/homepage/42.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/61.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/82.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/10.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/122.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/43.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/52.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/73.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/82.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/111.png'
-    
-    },
-    {
-        img: 'frontend/img/homepage/31.png'
-    
-    },
-      ]
- 
+            },
+            {
+                img: 'frontend/img/homepage/home3.jpg'
 
-           
-            $scope.homeImg = _.chunk($scope.homeImg, 12);
-            for (var i = 0; i < $scope.homeImg.length; i++) {
-                $scope.homeImg[i] = _.chunk($scope.homeImg[i], 6);
-                console.log($scope.homeImg);
+            },
+            {
+                img: 'frontend/img/homepage/home3.jpg'
+
+            },
+            {
+                img: 'frontend/img/homepage/home3.jpg'
+
+            },
+            {
+                img: 'frontend/img/homepage/home3.jpg',
+                titlehome2: 'THE TWO-DOLLAR BILL',
+                title2: 'BY CHRISTIAN GREWE'
             }
-            console.log("this is client");
+        ]
+
+        $scope.homeImg = [{
+                img: 'frontend/img/homepage/32.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/42.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/61.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/82.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/10.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/122.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/43.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/52.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/73.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/82.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/111.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/31.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/32.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/42.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/61.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/82.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/10.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/122.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/43.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/52.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/73.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/82.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/111.png'
+
+            },
+            {
+                img: 'frontend/img/homepage/31.png'
+
+            },
+        ]
+
+
+
+        $scope.homeImg = _.chunk($scope.homeImg, 12);
+        for (var i = 0; i < $scope.homeImg.length; i++) {
+            $scope.homeImg[i] = _.chunk($scope.homeImg[i], 6);
             console.log($scope.homeImg);
-    
+        }
+        console.log("this is client");
+        console.log($scope.homeImg);
 
 
-     $scope.homeText = [{
-             title1: 'PARLE, SR. PRODUCT MANAGER ',
-        title2: '“The feedback from the market as well as our field force has been quite encouraging.I must take this opportunity to mention that Procreate is a fast emerging design specialist firm with the most important trait of not just creativity, but has a lot of willingness to explore and walk that extra mile to deliver to the client’s satisfaction.I wish Akhilesh and team Procreate all the very best.”  '
-     },
-     {
-             title1: 'PARLE, SR. PRODUCT MANAGER',
-        title2: '“The feedback from the market as well as our field force has been quite encouraging.I must take this opportunity to mention that Procreate is a fast emerging design specialist firm with the most important trait of not just creativity, but has a lot of willingness to explore and walk that extra mile to deliver to the client’s satisfaction.I wish Akhilesh and team Procreate all the very best.”',
-        
-     },
-     
-     ]
+
+        $scope.homeText = [{
+                title1: 'PARLE, SR. PRODUCT MANAGER ',
+                title2: '“The feedback from the market as well as our field force has been quite encouraging.I must take this opportunity to mention that Procreate is a fast emerging design specialist firm with the most important trait of not just creativity, but has a lot of willingness to explore and walk that extra mile to deliver to the client’s satisfaction.I wish Akhilesh and team Procreate all the very best.”  '
+            },
+            {
+                title1: 'PARLE, SR. PRODUCT MANAGER',
+                title2: '“The feedback from the market as well as our field force has been quite encouraging.I must take this opportunity to mention that Procreate is a fast emerging design specialist firm with the most important trait of not just creativity, but has a lot of willingness to explore and walk that extra mile to deliver to the client’s satisfaction.I wish Akhilesh and team Procreate all the very best.”',
+
+            },
+
+        ]
 
 
         var abc = _.times(100, function (n) {
@@ -184,7 +185,26 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
             console.log("This is a button Click");
         };
 
+        initMap = function () {
+                var uluru = {lat: 19.166411, lng: 72.847466};
+            // Styles a map in night mode.
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center:uluru,
+                scrollwheel: false,
+                zoom: 15,
+            });
 
+            var marker = new google.maps.Marker({
+                position:uluru,
+                title: "Procreate",
+                // icon: "http://gsourcedata.com/img/landing-logo.png/",
+                map: map
+
+            });
+        }
+        $timeout(function () {
+            initMap();
+        }, 500);
 
     })
 
@@ -199,21 +219,21 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         };
     })
 
-        .controller('AboutCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+    .controller('AboutCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
         $scope.template = TemplateService.getHTML("content/about.html");
         TemplateService.title = "About"; //This is the Title of the Website
-         TemplateService.header = "views/template/header1.html"; //This is the Title of the Website
+        TemplateService.header = "views/template/header1.html"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
     })
 
-     .controller('OfferingCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+    .controller('OfferingCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
         $scope.template = TemplateService.getHTML("content/offering.html");
         TemplateService.title = "Offering"; //This is the Title of the Website
-           TemplateService.header = "views/template/header2.html";
+        TemplateService.header = "views/template/header2.html";
         $scope.navigation = NavigationService.getNavigation();
     })
 
-    
+
     //Example API Controller
     .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
         apiService.getDemo($scope.formData, function (data) {
