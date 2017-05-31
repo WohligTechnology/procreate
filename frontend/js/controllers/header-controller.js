0acf442f2ae7d5a4b99db1051e4778d558fdca71
sohan1 <shn619@gmail.com>
2017-05-31 19:48:22 +0530

    main.css

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,11 +1,11 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, TemplateService, $rootScope) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
-    $scope.changeURL = function (id) {
+        $scope.changeURL = function (id) {
       console.log(id);
-      $location.path("/offering/" + id);
+      $location.path("" + id);
     };
 });
