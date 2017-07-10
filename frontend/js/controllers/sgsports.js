b1f342f56745b4e2fa1af3c77a0dea2dc484a768
root <ganesh.wohlig@gmail.com>
2017-07-10 19:10:40 +0530

    done with sg pg

--- frontend/js/controllers/sgsports.js
+++ frontend/js/controllers/sgsports.js
@@ -2,6 +2,7 @@ myApp.controller('SgsportsCtrl', function ($scope, TemplateService, NavigationSe
         $scope.template = TemplateService.getHTML("content/sgsports.html");
         TemplateService.title = "sgsports"; //This is the Title of the Website
         //   TemplateService.header = "views/template/header2.html"; 
+           TemplateService.cssMain = "Vachan";
         $scope.navigation = NavigationService.getNavigation();
         $scope.formSubmitted = false;
       
