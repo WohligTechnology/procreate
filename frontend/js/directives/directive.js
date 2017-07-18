80e7b3f2a4ca2aedf88c41fa521a80f58b16872f
root <ganesh.wohlig@gmail.com>
2017-07-18 16:49:30 +0530

    home pg swiper

--- frontend/js/directives/directive.js
+++ frontend/js/directives/directive.js
@@ -1,24 +1,24 @@
-myApp.directive('img', function ($compile, $parse) {
-        return {
-            restrict: 'E',
-            replace: false,
-            link: function ($scope, element, attrs) {
-                var $element = $(element);
-                if (!attrs.noloading) {
-                    $element.after("<img src='img/loading.gif' class='loading' />");
-                    var $loading = $element.next(".loading");
-                    $element.load(function () {
-                        $loading.remove();
-                        $(this).addClass("doneLoading");
-                    });
-                } else {
-                    $($element).addClass("doneLoading");
-                }
-            }
-        };
-    })
+// myApp.directive('img', function ($compile, $parse) {
+//         return {
+//             restrict: 'E',
+//             replace: false,
+//             link: function ($scope, element, attrs) {
+//                 var $element = $(element);
+//                 if (!attrs.noloading) {
+//                     $element.after("<img src='img/loading.gif' class='loading' />");
+//                     var $loading = $element.next(".loading");
+//                     $element.load(function () {
+//                         $loading.remove();
+//                         $(this).addClass("doneLoading");
+//                     });
+//                 } else {
+//                     $($element).addClass("doneLoading");
+//                 }
+//             }
+//         };
+//     })
 
-    .directive('hideOnScroll', function ($document) {
+    myApp.directive('hideOnScroll', function ($document) {
         return {
             restrict: 'EA',
             replace: false,
