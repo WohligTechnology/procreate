3c78dd7ada768b86b0bbd82fee114dcf9189fae7
snehalthakare <snehal.thakare@wohlig.com>
2017-07-31 17:56:56 +0530

    mob responsive

--- frontend/js/controllers/contact.js
+++ frontend/js/controllers/contact.js
@@ -77,26 +77,12 @@ myApp.controller('ContactCtrl', function ($scope, $state, toastr, TemplateServic
                 if (data) {
                     console.log("data", data);
 
-                    goog_report_conversion = function (url) {
-                        goog_snippet_vars();
-                        window.google_conversion_format = "3";
-                        var opt = new Object();
-                        opt.onload_callback = function () {
-                            if (typeof (url) != 'undefined') {
-                                window.location = url;
-                            }
-                        }
-                        var conv_handler = window['google_trackConversion'];
-                        if (typeof (conv_handler) == 'function') {
-                            conv_handler(opt);
-                        }
-                    }
                 }
             });
             var url = $state.href('thankyou', {
                 parameter: "parameter"
             });
-            window.open(url, "popup","width=900,height=600,left=10,top=150");
+            window.open(url, "_blank");
         } else {
             console.log("******** inside sendMails *********", data);
             console.log("******** inside sendMail *********", data);
--- frontend/js/controllers/thankyou.js
+++ frontend/js/controllers/thankyou.js
@@ -6,4 +6,19 @@ myApp.controller('ThankYouCtrl', function ($scope, TemplateService, NavigationSe
         $scope.navigation = NavigationService.getNavigation();
         $scope.formSubmitted = false;
    
+                    goog_report_conversion = function (url) {
+                        goog_snippet_vars();
+                        window.google_conversion_format = "3";
+                        var opt = new Object();
+                        console.log("google");
+                        opt.onload_callback = function () {
+                            if (typeof (url) != 'undefined') {
+                                window.location = url;
+                            }
+                        }
+                        var conv_handler = window['google_trackConversion'];
+                        if (typeof (conv_handler) == 'function') {
+                            conv_handler(opt);
+                        }
+                    }
     })
